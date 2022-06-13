fe61d6a554b9bcee5ae491026d1d578ca6a6a81c
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-06-13 15:03:25 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -5,12 +5,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-# r = redis.Redis()
 redis_url = os.getenv('REDIS_URL')
 
-r = redis.from_url(redis_url)
+conn = redis.from_url(redis_url)
+# conn= redis.Redis()
 
-q = Queue(connection=r)
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))

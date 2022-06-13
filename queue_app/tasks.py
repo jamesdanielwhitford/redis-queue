62f9e36ac6f5918ec74c6ba1642cd861a3cb41d5
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2022-06-13 16:57:10 +0200

    Update tasks.py

--- queue_app/tasks.py
+++ queue_app/tasks.py
@@ -5,22 +5,31 @@ import time
 import redis 
 
 def count_words(url):
-
     time.sleep(1)
 
-    r = request.urlopen(url)
+    try:
+        r = request.urlopen(url)
 
-    if "bbc" in url:
-        tag_text = "p"
-        class_text = "ssrcss-1q0x1qg-Paragraph eq5iqo00"
-    elif "cnn" in url:
-        tag_text = "div"
-        class_text = "zn-body__paragraph" 
+        if "bbc" in url:
+            tag_text = "p"
+            class_text = "ssrcss-1q0x1qg-Paragraph eq5iqo00"
+        elif "cnn" in url:
+            tag_text = "div"
+            class_text = "zn-body__paragraph"
+        else:
+            tag_text="div"
+            class_text = None
 
-    soup = BeautifulSoup(r.read().decode(), "lxml")
+        soup = BeautifulSoup(r.read().decode(), "lxml")
 
-    title = soup.title.text
+        title = soup.title.text
 
-    paragraphs = [p.text for p in soup.find_all(tag_text, class_text)]
+        paragraphs = [p.text for p in soup.find_all(tag_text, class_text)]
 
-    return paragraphs, title
+        return paragraphs, title
+    
+    except:
+        paragraphs = ["The url you submitted is invalid or cannot be scraped."]
+        title = "Failed to access url"
+        return paragraphs, title
+
